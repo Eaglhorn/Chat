40c7be5e9a1efbf165a4e95b874ab6bf3545f5ea
Eaglhorn <korol.pasha1992@gmail.com>
2018-02-21 14:44:04 +0200

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBS2lmwRQtBeH0xEJ3ps06k7mB_5SKIkYw',
+    authDomain: 'chat-demo-f2b8f.firebaseapp.com',
+    databaseURL: 'https://chat-demo-f2b8f.firebaseio.com',
+    projectId: 'chat-demo-f2b8f',
+    storageBucket: 'chat-demo-f2b8f.appspot.com',
+    messagingSenderId: '481891005202'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBS2lmwRQtBeH0xEJ3ps06k7mB_5SKIkYw',
+    authDomain: 'chat-demo-f2b8f.firebaseapp.com',
+    databaseURL: 'https://chat-demo-f2b8f.firebaseio.com',
+    projectId: 'chat-demo-f2b8f',
+    storageBucket: 'chat-demo-f2b8f.appspot.com',
+    messagingSenderId: '481891005202'
+  }
+};
+
+/*create your db*/
